a2e72497e2c47e5ef1f0d1d91bc5df8b8d604be3
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-07-02 22:52:06 +0530

    Add number guessing functionality in quickguess.js
    Implement number guessing logic with user feedback.

---
+++ Day 02 - Guessing The Number/qucikguess.js
@@ -0,0 +1,21 @@
+let gameResult = document.getElementById("gameResult");
+let userInput = document.getElementById("userInput");
+let randomNumber = Math.ceil(Math.random() * 100);
+console.log(randomNumber);
+
+function checkGuess() {
+    let guessedNumber = parseInt(userInput.value);
+    if (guessedNumber > randomNumber) {
+        gameResult.textContent = "Too high! Try Again";
+        gameResult.style.backgroundColor = "#1e217c";
+    } else if (guessedNumber < randomNumber) {
+        gameResult.textContent = "Too low, Try Again";
+        gameResult.style.backgroundColor = "#1e217c";
+    } else if (guessedNumber === randomNumber) {
+        gameResult.textContent = "Good morning! 🌟 Talk about starting the day with a win. You got the right number! - your's budodu💖";
+        gameResult.style.backgroundColor = "green";
+    } else {
+        gameResult.textContent = "Please provide the valid input";
+        gameResult.style.backgroundColor = "red";
+    }
+}
