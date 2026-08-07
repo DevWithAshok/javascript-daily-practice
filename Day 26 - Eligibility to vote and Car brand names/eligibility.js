af0960f399f1077ccf092c8779d492b307fbb817
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-08-07 23:51:53 +0530

    Day 26 - Add eligibility check for voting based on age

---
+++ Day 26 - Eligibility to vote and Car brand names/eligibility.js
@@ -0,0 +1,37 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // arrayOfPersons
+  let arrayOfPersons = JSON.parse(readLine().replace(/'/g, '"'));
+
+  for (let i in arrayOfPersons){
+      let value=arrayOfPersons[i]
+      if (value['age']>=18){
+          console.log(value['name']);
+      }
+  }
+}
+
